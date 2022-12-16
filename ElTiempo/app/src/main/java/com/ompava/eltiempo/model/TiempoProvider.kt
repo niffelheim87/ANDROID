5c648b153989f56d71c8106367c65d6c205122bc
Niffelheim <omarkali@gmail.com>
2022-12-16 11:14:51 +0100

    update

--- ElTiempo/app/src/main/java/com/ompava/eltiempo/model/TiempoProvider.kt
+++ ElTiempo/app/src/main/java/com/ompava/eltiempo/model/TiempoProvider.kt
@@ -1,19 +1,4 @@
 package com.ompava.eltiempo.model
 
 class TiempoProvider {
-    companion object{
-
-        fun random():TiempoModel{
-            val position: Int = (0..3).random()
-            return tiempo[position]
-        }
-
-        private val tiempo = listOf<TiempoModel>(
-            TiempoModel(tiempo = "sun"),
-            TiempoModel(tiempo = "clouds"),
-            TiempoModel(tiempo = "rain"),
-            TiempoModel(tiempo = "wind")
-        )
-    }
-
 }
